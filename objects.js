971ce90449df8b503799629eb1d719044f1cb82a
Davideb94 <davide.db94@icloud.com>
2018-09-07 23:05:28 +0200

    :sparkles: Implemented levels structure.

---
+++ objects.js
@@ -0,0 +1,49 @@
+const objects = {
+
+  object_1: {
+
+    model: {
+      part1:{
+        number: 1,
+        img: ""
+      }
+      part2:{
+        number: 2,
+        img: ""
+      }
+      part3:{
+        number: 3,
+        img: ""
+      }
+      part4:{
+        number: 4,
+        img: ""
+      }
+    },
+
+    comp1:{
+      part: null,
+      img: ""
+    },
+    comp2:{
+      part: null,
+      img: ""
+    },
+    comp3:{
+      part: null,
+      img: ""
+    },
+    comp4:{
+      part: null,
+      img: ""
+    },
+    comp5:{
+      part: null,
+      img: ""
+    },
+    comp6:{
+      part: null,
+      img: ""
+    }
+
+}
